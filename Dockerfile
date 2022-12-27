1952e89805224bb15f3ba25ca7ebcbccae999751
Estel Smith <estel.smith@gmail.com>
2022-12-27 13:26:21 -0500

    add initial build config

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+FROM registry.home.estelsmith.com/alpine:3.17
+
+RUN apk --no-cache add tini jq gettext transmission-cli transmission-daemon
+COPY entrypoint.sh /entrypoint.sh
+COPY default-settings.json /default-settings.json
+COPY default-slurp.json /default-slurp.json
+
+RUN adduser -s /sbin/nologin -h /app -H -D appuser
+
+RUN <<EOF
+  chmod 0700 /entrypoint.sh
+  for i in config torrents incomplete download; do
+    mkdir -p "/app/${i}"
+    chown appuser:appuser "/app/${i}"
+  done
+EOF
+
+VOLUME /app/config
+VOLUME /app/torrents
+VOLUME /app/incomplete
+VOLUME /app/download
+
+USER appuser:appuser
+WORKDIR /app
+ENTRYPOINT ["/sbin/tini", "--", "/entrypoint.sh"]
+CMD []
---
+++ entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -e
+
+CONFIG_DIR="/app/config"
+
+DEFAULT_SLURP_PATH="/default-slurp.json"
+SLURP_PATH="${CONFIG_DIR}/slurp.json"
+DEFAULT_SETTINGS_PATH="/default-settings.json"
+SETTINGS_PATH="${CONFIG_DIR}/settings.json"
+
+if [ ! -f "${SETTINGS_PATH}" ]; then
+    cp "${DEFAULT_SETTINGS_PATH}" "${SETTINGS_PATH}"
+fi
+
+cat "${DEFAULT_SLURP_PATH}" | envsubst > "${SLURP_PATH}"
+
+jq --slurpfile slurp "${SLURP_PATH}" '. + $slurp[]' "${SETTINGS_PATH}" > "${SETTINGS_PATH}2"
+mv "${SETTINGS_PATH}2" "${SETTINGS_PATH}"
+rm "${SLURP_PATH}"
+
+exec /usr/bin/transmission-daemon \
+    --foreground \
+    --config-dir "/app/config" \
+    --watch-dir "/app/torrents" \
+    --logfile /dev/stdout \
+    "$@"
